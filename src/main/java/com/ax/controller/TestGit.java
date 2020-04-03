bb3d029f6504bab187f8b4487995aa84cc29e8a1
sl <shenlei_friend@163.com>
2020-04-03 14:42:38 +0800

    edit hot_fix

--- src/main/java/com/ax/controller/TestGit.java
+++ src/main/java/com/ax/controller/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("testBBB");
 		System.out.println("testAAAA");
+		System.out.println("-----------hot_fix-------------");
 	}
 }

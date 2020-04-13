1f7d31b6c10521570bc5cee3b5bc75d48be99677
sl <shenlei_friend@163.com>
2020-04-13 13:33:48 +0800

    测试拦截器

---
+++ src/main/java/com/sl/interceptor/MyInterceptor3.java
@@ -0,0 +1,29 @@
+package com.sl.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MyInterceptor3 extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+							 Object handler) throws Exception {
+		System.out.println("action之前执行3");
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response,
+						   Object handler, ModelAndView modelAndView) throws Exception {
+		System.out.println("action之后，生成视图之前执行3");
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+								Object handler, Exception ex) throws Exception {
+		System.out.println("最后执行，释放资源，处理异常3");
+	}
+}
